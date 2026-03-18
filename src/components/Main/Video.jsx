0f9f233f2cfb8f9276f9e2a0288e5d203954ab83
Abdallha <AT010Abdallha@gmail.com>
2026-03-19 01:36:57 +0200

    Finish ChooseUs section

--- src/components/Main/Video.jsx
+++ src/components/Main/Video.jsx
@@ -4,18 +4,17 @@ const Video = () => {
   return (
     <section className="w-full h-screen relative overflow-hidden">
       
-      {/* Video */}
-      <iframe
-        className="absolute top-0 left-0 w-full h-full object-cover"
-        src="https://www.youtube.com/embed/85kTHwJ1Ju8?autoplay=1&mute=1&loop=1&playlist=85kTHwJ1Ju8&controls=0&showinfo=0&modestbranding=1"
-        title="Background Video"
-        allow="autoplay; fullscreen"
-      ></iframe>
+      {/* Video Wrapper */}
+      <div className="absolute inset-0 overflow-hidden">
+        <iframe
+          className="absolute top-1/2 left-1/2 w-[177.77vh] h-[100vw] min-w-full min-h-full -translate-x-1/2 -translate-y-1/2"
+          src="https://www.youtube.com/embed/85kTHwJ1Ju8?autoplay=1&mute=1&controls=0&modestbranding=1&loop=1&playlist=85kTHwJ1Ju8"
+          title="Background Video"
+          allow="autoplay; fullscreen"
+        ></iframe>
+      </div>
 
-      {/* Dark overlay */}
-      <div className="absolute inset-0 bg-black/50"></div>
-
-      {/* Content on top */}
+      {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
         <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold mb-4">
           Welcome to Agrica
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TipsSection from '../../components/Main/TipsSection'
 import HelpSection from '../../components/Main/HelpSection'
-import Video from '../../components/Main/Video'
+import Video from '../../components//Main/Video'
+import ChooseUs from '../../components/Main/ChooseUs'
 import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
     <main>
+      <ChooseUs />
       <Video />
       <TipsSection />
       <HelpSection />
